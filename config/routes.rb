984c4d5fc1bfee003bd426eb7fbfda38eeb4359a
liamdarmody <liamdarmody@gmail.com>
2015-03-30 01:03:20 +1100

    Added Styling Updates - Particularly to Devise Forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Growthpal::Application.routes.draw do
-  
+
   root "pages#home"
   get "home", to: "pages#home", as: "home"
+
   get "inside", to: "pages#inside", as: "inside"
   get "/contact", to: "pages#contact", as: "contact"
   post "/emailconfirmation", to: "pages#email", as: "email_confirmation"
